766d24d14808e5668c8a148dff4196daffe03d31
Adam G <okay_great@hotmail.com>
2016-09-27 22:13:10 -0700

    Initial Commit

---
+++ GameCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameCollector
+//
+//  Created by Adam Gendle on 9/27/16.
+//  Copyright © 2016 Adam Gendle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
